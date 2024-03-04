f383120f822acdd8737f4437e3bc910862801699
Divya Patel <162137928+Divya04Patel@users.noreply.github.com>
2024-03-04 19:28:30 +0530

    Add files via upload
    📝 **To-Do List App**
    
    ✨ **Features:**
    - **Add Tasks:** Quickly add tasks with a simple interface.
    - **Remove Tasks:** Effortlessly delete tasks you no longer need.
    - **Mark as Done:** Check off completed tasks to track your progress.
    
    🚀 **Why this app?**
    - **Simplicity:** Focus on what needs to be done without distractions.
    - **Intuitive Design:** User-friendly interface for seamless task management.
    - **Efficiency:** Streamline your workflow with easy task manipulation.
    
    🌐 **Tech Stack:**
    - **Frontend:** HTML, CSS, JavaScript
    
    🛠️ **How to Use:**
    1. Clone the repository to your local machine.
    2. Open `index.html` in your web browser.
    3. Start adding, removing, and marking tasks done!
    
    👩‍💻 **Contributions:**
    - Contributions are welcome! Fork the repository and submit pull requests for improvements.
    - Report issues if you encounter any bugs or have suggestions for enhancements.
    
    🔗 **Demo:**
    - [http://127.0.0.1:5500/index.html]
    
    Let's make task management a breeze! 🚀

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+        inputBox.value = "";
+
+        li.addEventListener("click", function () {
+            li.classList.toggle("checked");
+        });
+
+        span.addEventListener("click", function () {
+            li.remove();
+        });
+    }
+}
+
